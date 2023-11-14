d2a69eb00bb70d2b460f52822e6e7ac12c1a2fa8
loadfms <loadfms@gmail.com>
2023-11-14 11:21:44 -0300

    feat: update

--- polybar/.config/polybar/open-weather.sh
+++ polybar/.config/polybar/open-weather.sh
@@ -16,20 +16,10 @@ if [ -n "$CITY" ]; then
     fi
 
     weather=$(curl -sf "$API/weather?appid=$KEY&$CITY_PARAM&units=$UNITS")
-else
-    location=$(curl -sf "https://location.services.mozilla.com/v1/geolocate?key=geoclue")
-
-    if [ -n "$location" ]; then
-        location_lat="$(echo "$location" | jq '.location.lat')"
-        location_lon="$(echo "$location" | jq '.location.lng')"
-
-        weather=$(curl -sf "$API/weather?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS")
-    fi
 fi
 
 if [ -n "$weather" ]; then
     weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
-    weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
 
     echo "" "$weather_temp$SYMBOL"
 fi
