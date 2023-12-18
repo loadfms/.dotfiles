366939c3420277a770d78378f3ee475c8026eacc
loadfms <loadfms@gmail.com>
2023-12-18 15:39:42 -0300

    feat: update

--- nvim/.config/nvim/lua/lspConfig.lua
+++ nvim/.config/nvim/lua/lspConfig.lua
@@ -8,7 +8,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 require('mason').setup()
 require('mason-lspconfig').setup({
     ensure_installed = {
-        'eslint',
         'gopls',
         'html',
         'jsonls',
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
-
     { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
     'nvim-lua/plenary.nvim',
     'tpope/vim-fugitive',
