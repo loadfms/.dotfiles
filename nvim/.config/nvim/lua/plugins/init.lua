d1bf58d096b74f8a146ac7c352367d991f88f5a1
loadfms <loadfms@gmail.com>
2024-02-19 15:58:07 -0300

    feat: update

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,6 +1,6 @@
 return {
     --theme
-    { "olivercederborg/poimandres.nvim", priority = 1000 },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
     'nvim-lua/plenary.nvim',
     'tpope/vim-fugitive',
