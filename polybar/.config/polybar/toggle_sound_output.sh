78c8b8d50fe39b4a0a86885d6229caac2b4164b0
loadfms <loadfms@gmail.com>
2024-05-09 10:53:48 -0300

    feat: update

--- polybar/.config/polybar/toggle_sound_output.sh
+++ polybar/.config/polybar/toggle_sound_output.sh
@@ -19,11 +19,11 @@ case "$1" in
     # Toggle between commands A and B
     if [ "$current_state" = "A" ]; then
         # Execute command A
-        pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
+        pactl set-default-sink alsa_output.pci-0000_00_1f.3.3.analog-stereo
         echo "B" > /tmp/toogle_sound_output.txt
     else
         # Execute command B
-        pactl set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo-extra3
+        pactl set-default-sink alsa_output.pci-0000_01_00.1.3.hdmi-stereo-extra3
         echo "A" > /tmp/toogle_sound_output.txt
     fi
 esac
