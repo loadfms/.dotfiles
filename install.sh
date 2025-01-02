45d16cc2bffa004faef0ff31455f93fa4ef67abd
loadfms <loadfms@gmail.com>
2025-01-02 14:13:39 -0300

    feat: update

--- install.sh
+++ install.sh
@@ -10,19 +10,19 @@ do
 
 done
 
-while IFS=";" read -r source_path destination_path; do
-    source_path="$(echo "${source_path}" | tr -d '[:space:]')"
-    destination_path="$(echo "${destination_path}" | tr -d '[:space:]')"
+#while IFS=";" read -r source_path destination_path; do
+    #source_path="$(echo "${source_path}" | tr -d '[:space:]')"
+    #destination_path="$(echo "${destination_path}" | tr -d '[:space:]')"
 
-    override_file="$HOME/.override/$MACHINE_NAME/$source_path"
-    default_file="$HOME/.override/default/$source_path"
+    #override_file="$HOME/.override/$MACHINE_NAME/$source_path"
+    #default_file="$HOME/.override/default/$source_path"
 
-    # Check if the source file exists before copying
-    if [ -e "$override_file" ]; then
-        cp "$override_file" "$HOME/$destination_path"
-        echo "Copied '$override_file' to '$destination_path'"
-    else
-        cp "$default_file" "$HOME/$destination_path"
-        echo "Copied '$default_file' to '$destination_path'"
-    fi
-done < override-config
+    ## Check if the source file exists before copying
+    #if [ -e "$override_file" ]; then
+        #cp "$override_file" "$HOME/$destination_path"
+        #echo "Copied '$override_file' to '$destination_path'"
+    #else
+        #cp "$default_file" "$HOME/$destination_path"
+        #echo "Copied '$default_file' to '$destination_path'"
+    #fi
+#done < override-config
