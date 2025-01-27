34dd8b8913bf620427a22802d7761bd3faa04b15
loadfms <loadfms@gmail.com>
2025-01-27 17:28:03 -0300

    feat: update

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -120,6 +120,13 @@ return {
         "ibhagwan/fzf-lua",
         -- optional for icon support
         dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            previewers = {
+                builtin = {
+                    syntax_limit_b = 1024 * 100, -- 100KB
+                },
+            },
+        },
         config = function()
             -- calling `setup` is optional for customization
             require("fzf-lua").setup({
