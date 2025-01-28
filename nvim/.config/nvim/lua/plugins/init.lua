440099e91f5ba11dfec33bc6e5f644ee1af73a8e
loadfms <loadfms@gmail.com>
2025-01-28 09:56:09 -0300

    feat: update

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -3,6 +3,7 @@ return {
     --theme
     { "sainnhe/gruvbox-material" },
     { "norcalli/nvim-colorizer.lua", config = function() require("colorizer").setup() end },
+    { "atelierbram/Base2Tone-nvim" },
 
     {
         'github/copilot.vim',
--- nvim/.config/nvim/lua/theme.lua
+++ nvim/.config/nvim/lua/theme.lua
@@ -1,2 +1,2 @@
-vim.cmd.colorscheme "gruvbox-material"
+vim.cmd.colorscheme "base2tone_suburb_dark"
 --vim.cmd "hi Normal ctermbg=NONE guibg=NONE"
