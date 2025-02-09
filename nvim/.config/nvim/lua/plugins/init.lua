d90cca840957635c5e23b9a2c94bfb2beadafefb
loadfms <loadfms@gmail.com>
2025-02-09 13:46:06 -0300

    feat: update

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -39,8 +39,8 @@ return {
                     changedelete = { text = '~' },
                     untracked    = { text = '┆' },
                 },
-                signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
-                numhl                        = true,  -- Toggle with `:Gitsigns toggle_numhl`
+                signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+                numhl                        = true, -- Toggle with `:Gitsigns toggle_numhl`
                 linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
                 word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
                 watch_gitdir                 = {
@@ -59,7 +59,7 @@ return {
                 current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
                 sign_priority                = 6,
                 update_debounce              = 100,
-                status_formatter             = nil,   -- Use default
+                status_formatter             = nil, -- Use default
                 max_file_length              = 40000, -- Disable if file is longer than this (in lines)
                 preview_config               = {
                     -- Options passed to nvim_open_win
@@ -130,8 +130,7 @@ return {
             -- calling `setup` is optional for customization
             require("fzf-lua").setup({
                 files      = {
-                    fd_opts =
-                    [[--color=never --type f --hidden --follow --exclude .git --exclude node_modules ]],
+                    rg_opts = [[--files --hidden --glob !.git --glob !node_modules]]
                 },
                 previewers = {
                     builtin = {
