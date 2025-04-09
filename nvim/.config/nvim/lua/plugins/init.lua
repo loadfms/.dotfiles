1217c973188fbb1fab97be6dfa5eb876c0823038
loadfms <loadfms@gmail.com>
2025-04-09 15:21:32 -0300

    feat: update

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -26,94 +26,90 @@ return {
     },
     {
         'stevearc/oil.nvim',
+        cmd = "Oil",
         opts = {}
     },
     {
         'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup {
-                signs                        = {
-                    add          = { text = '┃' },
-                    change       = { text = '┃' },
-                    delete       = { text = '-' },
-                    topdelete    = { text = '‾' },
-                    changedelete = { text = '~' },
-                    untracked    = { text = '┆' },
-                },
-                signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
-                numhl                        = true,  -- Toggle with `:Gitsigns toggle_numhl`
-                linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
-                word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
-                watch_gitdir                 = {
-                    follow_files = true
-                },
-                auto_attach                  = true,
-                attach_to_untracked          = false,
-                current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-                current_line_blame_opts      = {
-                    virt_text = false,
-                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-                    delay = 1000,
-                    ignore_whitespace = false,
-                    virt_text_priority = 100,
-                },
-                current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-                sign_priority                = 6,
-                update_debounce              = 100,
-                status_formatter             = nil,   -- Use default
-                max_file_length              = 40000, -- Disable if file is longer than this (in lines)
-                preview_config               = {
-                    -- Options passed to nvim_open_win
-                    border = 'rounded',
-                    style = 'minimal',
-                    relative = 'cursor',
-                    row = 0,
-                    col = 1
-                },
-            }
-        end
+        cmd = "Git",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            signs                        = {
+                add          = { text = '┃' },
+                change       = { text = '┃' },
+                delete       = { text = '-' },
+                topdelete    = { text = '‾' },
+                changedelete = { text = '~' },
+                untracked    = { text = '┆' },
+            },
+            signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+            numhl                        = true,  -- Toggle with `:Gitsigns toggle_numhl`
+            linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+            word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+            watch_gitdir                 = {
+                follow_files = true
+            },
+            auto_attach                  = true,
+            attach_to_untracked          = false,
+            current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts      = {
+                virt_text = false,
+                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                delay = 1000,
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+            },
+            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+            sign_priority                = 6,
+            update_debounce              = 100,
+            status_formatter             = nil,   -- Use default
+            max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+            preview_config               = {
+                -- Options passed to nvim_open_win
+                border = 'rounded',
+                style = 'minimal',
+                relative = 'cursor',
+                row = 0,
+                col = 1
+            },
+        }
     },
     {
         "ibhagwan/fzf-lua",
+        cmd = "FzfLua",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
-            previewers = {
-                builtin = {
-                    syntax_limit_b = 1024 * 100, -- 100KB
+            winopts    = {
+                border  = "single",
+                preview = {
+                    border = "single",
                 },
             },
-        },
-        config = function()
-            require("fzf-lua").setup({
-                winopts    = {
-                    border  = "single",
-                    preview = {
-                        border = "single",
+            files      = {
+                rg_opts = [[--files --hidden --glob !.git --glob !node_modules]]
+            },
+            grep       = {
+                rg_opts =
+                "--color=never --no-heading --with-filename --line-number --column --smart-case --glob !node_modules",
+            },
+            previewers = {
+                builtin = {
+
+                    syntax_limit_b = 1024 * 100, -- 100KB
+                    extensions = {
+                        ["jpg"] = { "chafa", "{file}" },
                     },
-                },
-                files      = {
-                    rg_opts = [[--files --hidden --glob !.git --glob !node_modules]]
-                },
-                grep       = {
-                    rg_opts =
-                    "--color=never --no-heading --with-filename --line-number --column --smart-case --glob !node_modules",
-                },
-                previewers = {
-                    builtin = {
-                        extensions = {
-                            ["jpg"] = { "chafa", "{file}" },
-                        },
-                    }
                 }
-            })
-        end
+            }
+        }
     },
     {
         'nvim-treesitter/nvim-treesitter',
+        event = { "BufReadPost", "BufNewFile" },
         build = ':TSUpdate',
         config = function()
             require 'nvim-treesitter.configs'.setup {
-                ensure_installed = { "go", "lua", "javascript", "html", "typescript", "css", "scss", "tsx" },
+                ensure_installed = { "go", "lua" },
 
                 -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
@@ -142,9 +138,11 @@ return {
             }
         end,
     },
-    { 'neovim/nvim-lspconfig' },
+    { 'neovim/nvim-lspconfig',   event = "BufReadPre" },
     {
         'stevearc/conform.nvim',
+
+        event = "BufWritePre",
         opts = {},
         config = function()
             require("conform").setup({
