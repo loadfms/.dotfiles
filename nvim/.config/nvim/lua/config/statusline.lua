b8ccc87a71aa610e9abeb7d3cb7d83adc4406c7a
loadfms <loadfms@gmail.com>
2025-06-06 11:16:39 -0300

    feat: update

--- nvim/.config/nvim/lua/config/statusline.lua
+++ nvim/.config/nvim/lua/config/statusline.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 -- Set custom highlight for the statusline
-vim.cmd [[
-  highlight StatusLine guibg=#282828 guifg=#ebdbb2
-  highlight ModeMsg gui=bold guifg=#1b1b1b guibg=#a9b665
-]]
+-- vim.cmd [[
+--   highlight StatusLine guibg=#282828 guifg=#ebdbb2
+--   highlight ModeMsg gui=bold guifg=#1b1b1b guibg=#a9b665
+-- ]]
 
+-- Function to get the current mode and filetype icon
 M.get_mode = function()
     local mode_map = {
         n = "NORMAL",
@@ -16,23 +17,22 @@ M.get_mode = function()
         c = "COMMAND",
         s = "SELECT"
     }
-    return mode_map[vim.api.nvim_get_mode().mode] or "UNKNOWN"
+    local mode = vim.api.nvim_get_mode().mode
+    return mode_map[mode] or "UNKNOWN"
 end
 
--- M.get_git_branch = function()
---     local branch = vim.fn.systemlist("git branch --show-current 2>/dev/null")[1]
---     local icon = "  "
---     return branch and #branch > 0 and (icon .. branch) or "No Git"
--- end
+M.get_filetype_icon = function()
+    local ft = vim.bo.filetype
+    local icon, _ = require('nvim-web-devicons').get_icon_by_filetype(ft, { default = true })
+    return icon or ""
+end
 
+-- Function to set up the statusline
 function M.setup()
     vim.o.statusline = table.concat({
         "%#ModeMsg# ", "%{v:lua.require'config.statusline'.get_mode()} ",
-
-        -- "%#StatusLine# ", "%{v:lua.require'statusline'.get_git_branch()} ",
         "%#StatusLine# %f%{&modified ? ' ●' : ''}%{&readonly ? ' ' : ''}%{&buftype == 'nofile' ? ' 󰊠' : ''} ",
-        "%=%#StatusLine# [%{&fileencoding}] [%{&fileformat}] %y ",
-
+        "%=%#StatusLine# %{v:lua.require'config.statusline'.get_filetype_icon()} ",
         "%3l:%3c"
     })
 end
